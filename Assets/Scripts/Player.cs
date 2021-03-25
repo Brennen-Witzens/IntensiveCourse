a727507c2d6d8581c05c9e37bba893a0320544cb
Brennen <bwitzen@irace.com>
2021-03-25 08:10:37 -0700

    Getting user input and movement implemented

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField]
+    private float _speed;
+    private float _horizontalInput;
+    private float _verticalInput;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(_horizontalInput, _verticalInput, 0) * (_speed * Time.deltaTime);
+        transform.Translate(movement);
+
+
+    }
+}
